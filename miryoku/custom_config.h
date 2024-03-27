993642dbd8759448e188d5cb2a70c9e00e23ad64
Inon Prince <inon.prince@oracle.com>
2024-03-27 11:40:17 -0700

    initial inoniofication

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_DVORAK
+#define MIRYOKU_EXTRA_COLEMAKDH
+#define MIRYOKU_TAP_DVORAK
+#define MIRYOKU_CLIPBOARD_MAC
+#define MIRYOKU_LAYERS_FLIP
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR
